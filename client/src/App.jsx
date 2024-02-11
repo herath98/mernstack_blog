435dfe19658e83b0a73e6699c382989b6a2e199f
harsha <harshaudayanga401@gmail.com>
2024-02-11 14:14:30 +0530

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-// import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SingIn from './pages/SingIn'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
 
 function App() {
 
 
   return (
-    <>
-     
-      <h1 className='text-3xl' >Vite + React</h1>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='\' element={<Home />}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='sign-up' element={<SignUp/>}/>
+        <Route path='sign-in' element={<SingIn/>}/>
+        <Route path='dashboard' element={<Dashboard/>}/>
+        <Route path='project' element={<Project/>}/>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,7 @@
+
+
+export default function About() {
+  return (
+    <div className="text-lg text-blue-500" >About</div>
+  )
+}
