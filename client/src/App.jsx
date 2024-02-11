875a43b52261bb6a096e101b2c3f0d256e2f6206
harsha <harshaudayanga401@gmail.com>
2024-02-11 21:17:41 +0530

    nvbar create using flowbite react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,22 @@ import SignUp from './pages/SignUp'
 import SingIn from './pages/SingIn'
 import Dashboard from './pages/Dashboard'
 import Project from './pages/Project'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='\' element={<Home />}/>
-        <Route path='about' element={<About/>}/>
-        <Route path='sign-up' element={<SignUp/>}/>
-        <Route path='sign-in' element={<SingIn/>}/>
-        <Route path='dashboard' element={<Dashboard/>}/>
-        <Route path='project' element={<Project/>}/>
-        
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='sign-in' element={<SingIn />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='project' element={<Project />} />
+
       </Routes>
     </BrowserRouter>
   )
