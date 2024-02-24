6bd5b62ec1bdf2d5c99c77086079c7cfd5cfe94f
harsha <harshaudayanga401@gmail.com>
2024-02-24 23:54:14 +0530

    commit

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
-
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSlidebar from "../components/DashSlidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab , setTab] = useState('');
+  useEffect (()=>{
+    const urlprams = new URLSearchParams(location.search);
+    const tabFormUrl= urlprams.get('tab');
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen  flex flex-col md:flex-row ">
+      <div className="md:w-56" >
+          {/* Slidebar */}
+          <DashSlidebar />
+      </div>
+          {/* profile... */}
+          {tab === 'profile' && <DashProfile/>}
+    </div>
   )
 }
